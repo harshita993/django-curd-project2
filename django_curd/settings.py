f2546eb65b75a077baaa9c8d7186f93a99cb476d
harshita993 <harshita.lakkad@gmail.com>
2025-01-16 12:08:52 +0530

    add project 2

--- django_curd/settings.py
+++ django_curd/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
+    
 ]
 
 ROOT_URLCONF = 'django_curd.urls'
@@ -55,14 +57,14 @@ ROOT_URLCONF = 'django_curd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.messages'
             ],
         },
     },
--- enroll/views.py
+++ enroll/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from .forms import student_registration
 from .models import user
+from django.contrib import messages
 # Create your views here.
 def add_show(request):
     if request.method=='POST':
@@ -12,7 +13,8 @@ def add_show(request):
             pw=fm.cleaned_data['password']
             reg=user(name=nm,email=em,password=pw)
             reg.save()'''
-            fm.save()
+            new_stu= fm.save()
+            messages.success(request,f"{new_stu.name} was successfully added")
         fm=student_registration()
         data=user.objects.all() 
     else:
@@ -24,6 +26,7 @@ def delete_data(request,id):
     if request.method=='POST':
         pi=user.objects.get(pk=id)
         pi.delete()
+        messages.success(request,f"{pi.name} was successfully deleted")
         return redirect('/')
     return render(request,'addandshow.html')
 def update_data(request,id):
@@ -32,7 +35,9 @@ def update_data(request,id):
         fm=student_registration(request.POST,instance=pi)
         if fm.is_valid():
            fm.save()
+           messages.success(request,f"{pi.name} was successfully updated")
         fm=student_registration()
+        return redirect('addshow')
          
     else:
         pi=user.objects.get(pk=id)
